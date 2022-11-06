f5b0e8ab472336b63f29d188f186a3d03425bec5
FastShowDev <zlFastShow@hotmail.com>
2022-11-06 16:12:13 -0300

    Add: Script for Enemy Moviment

---
+++ scenes/enemy/enemy_template.gd
@@ -0,0 +1,51 @@
+extends KinematicBody2D
+class_name EnemyTemplate
+
+onready var texture: Sprite = get_node("Texture")
+onready var floor_ray: RayCast2D = get_node("FloorCast")
+onready var animation: AnimationPlayer = get_node("Animation")
+
+var can_die: bool = false
+var can_hit: bool = false
+var can_attack: bool = false
+
+var velocity: Vector2
+var player_ref: Player = null
+
+export(int) var speed
+export(int) var gravity_speed
+export(int) var proximity_threshold
+
+func _physics_process(delta: float) -> void:
+	gravity(delta)
+	move_behavior()
+
+func gravity(delta: float) -> void:
+	velocity.y += delta * gravity_speed
+
+
+func move_behavior() -> void:
+	if player_ref != null:
+		var distance: Vector2 = player_ref.global_position - self.global.position
+		var direction: Vector2 = distance.normalized()
+		
+		if abs(distance.x) <= proximity_threshold:
+			velocity.x = 0
+			can_attack = true
+		elif floor_collision() and not can_attack:
+			velocity.x = direction.x * speed
+		else:
+			velocity.x = 0
+		return
+	velocity.x = 0
+
+
+func floor_collision() -> bool:
+	if floor_ray.is_colliding():
+		return true
+		
+	return false
+
+
+func _ready():
+	pass
--- scripts/enemy/detection_area.gd
+++ scripts/enemy/detection_area.gd
@@ -4,11 +4,9 @@ class_name DetectionArea
 export(NodePath) onready var enemy = get_node(enemy) as KinematicBody2D
 
 
-
-
 func _on_DetectionArea_body_entered(body: Player) -> void:
 	enemy.player_ref = body
 
 
 func _on_DetectionArea_body_exited(body):
-	pass # Replace with function body.
+	enemy.player_ref = null
