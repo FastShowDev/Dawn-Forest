1ab50d9ccb6b45d2e7ca49711848363c97557dea
FastShowDev <zlFastShow@hotmail.com>
2022-11-06 12:43:02 -0300

    Add: Script for Physics and Logic in Wall Slide for Player

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -3,24 +3,31 @@ class_name Player
 
 #References:
 onready var player_sprite: Sprite = get_node("Texture")
+onready var wall_ray: RayCast2D = get_node("WallRay")
 
 #Player physics:
 var velocity: Vector2
 export(int) var speed = 100
-var jump_count: int = 0
+export(int) var jump_speed
+export(int) var player_gravity
 
+export(int) var wall_jump_speed
+export(int) var wall_gravity
+export(int) var wall_impulse_speed
+
+var direction: int = 1
+var jump_count: int = 0
 
 #Flags
 var landing: bool = false
+var on_wall: bool = false
 var attacking: bool = false
 var defending: bool = false
 var crouching: bool = false
 
-#Flag permisions:
 var can_track_input: bool = false
+var not_on_wall: bool = true
 
-export(int) var jump_speed
-export(int) var player_gravity
 
 func _physics_process(delta: float):
 	horizontal_movement_env()
@@ -45,13 +52,17 @@ func horizontal_movement_env() -> void:
 	
 func vertical_moviment_env():
 	#Verifica se o player está no chão e reseta o jump_count
-	if is_on_floor():
+	if is_on_floor() or is_on_wall():
 		jump_count = 0
 		
 	var jump_condition: bool = can_track_input and not attacking
 	if Input.is_action_just_pressed("jump") and jump_count < 2 and jump_condition:
 		jump_count += 1
-		velocity.y = jump_speed
+		if next_to_wall() and not is_on_floor():
+			velocity.y = wall_jump_speed
+			velocity.x += wall_impulse_speed * direction
+		else:
+			velocity.y = jump_speed
 
 
 func animate(velocity: Vector2) -> void:
@@ -91,7 +102,27 @@ func defense() -> void:
 
 
 func gravity(delta: float) -> void:
-	velocity.y += delta * player_gravity
-	#Limita a velocidade máxima na vertical em quedas longas
-	if velocity.y >= player_gravity:
-		velocity.y = player_gravity
+	if next_to_wall():
+		velocity.y += wall_gravity * delta
+		#Limita a velocidade máxima na parede
+		if velocity.y >= wall_gravity:
+			velocity.y = wall_gravity
+	else:
+		velocity.y += delta * player_gravity
+		#Limita a velocidade máxima na vertical em quedas longas
+		if velocity.y >= player_gravity:
+			velocity.y = player_gravity
+
+
+func next_to_wall() -> bool:
+	if wall_ray.is_colliding() and not is_on_floor():
+		if not_on_wall:
+			velocity.y = 0
+			not_on_wall = false
+			
+		return true
+		
+	else:
+		not_on_wall = true
+		return false
+		
