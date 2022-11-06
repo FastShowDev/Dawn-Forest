ce9c96aa3cca6e3d882afe01969e3c87d080388c
FastShowDev <zlFastShow@hotmail.com>
2022-11-06 18:18:38 -0300

    Add: Collision and Atributes for Whale, add detection area shape, add script for playing the animation

---
+++ scripts/enemy/whale/whale_texture.gd
@@ -0,0 +1,14 @@
+extends EnemyTexture
+class_name WhaleTexture
+
+func animate(velocity: Vector2) -> void:
+	move_behavior(velocity)
+	
+
+func move_behavior(velocity: Vector2) -> void:
+	if velocity.x != 0:
+		animation.play("run")
+	else:
+		animation.play("idle")
+
+
