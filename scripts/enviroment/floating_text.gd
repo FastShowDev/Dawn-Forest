b314ade3d57f03f210008f215981480757053238
FastShowDev <zlFastShow@hotmail.com>
2022-11-15 18:11:56 -0300

    Add: Script that interpolate and inicialize the popup text

--- scripts/enviroment/floating_text.gd
+++ scripts/enviroment/floating_text.gd
@@ -16,3 +16,72 @@ export(Color) var exp_color
 export(Color) var heal_color
 export(Color) var mana_color
 export(Color) var damage_color
+
+func _ready() -> void:
+	randomize()
+	#Inicial random impulse
+	velocity = Vector2(
+		rand_range(-10,10),
+		-30
+	)
+	
+	floating_text()
+	
+func floating_text() -> void:
+	#Modifica a cor do texto com base no type:
+	text = type_sign + str(value)
+	match type:
+		"Exp":
+			modulate = exp_color
+		"Heal":
+			modulate = heal_color
+		"Mana":
+			modulate = mana_color
+		"Damage":
+			modulate = damage_color
+	interpolate()
+
+func interpolate() -> void:
+	#Diminui a visibilidade
+	var _interpolate_modulate = tween.interpolate_property(
+		self,
+		"modulate:a",
+		1.0,
+		0.0,
+		0.3,
+		Tween.TRANS_LINEAR,
+		Tween.EASE_OUT,
+		0.7
+	)
+	#Invoca o label aumentando a escala
+	var _interpolate_scale_up = tween.interpolate_property(
+		self,
+		"react_scale",
+		Vector2(0.0, 0.0),
+		Vector2(1.0, 1.0),
+		0.3,
+		Tween.TRANS_LINEAR,
+		Tween.EASE_OUT
+	)
+	#Diminui a escala dele
+	var _interpolate_scale_down = tween.interpolate_property(
+		self,
+		"react_scale",
+		Vector2(1.0, 1.0),
+		Vector2(0.4, 0.4),
+		1.0,
+		Tween.TRANS_LINEAR,
+		Tween.EASE_OUT,
+		0.6
+	)
+	
+	var _start = tween.start()
+	yield(tween, "tween_all_completed")
+	queue_free()
+	
+
+func _process(delta: float) -> void:
+	#Aplica o impulso inicial
+	velocity += gravity * mass * delta
+	rect_position += velocity * delta
+
