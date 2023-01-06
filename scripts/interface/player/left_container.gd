19122de5b48cd2fc175db47d254cb1ef16e67e92
FastShowDev <zlFastShow@hotmail.com>
2023-01-06 16:05:30 -0300

    Create functions for update the text of stats and bonus stats

--- scripts/interface/player/left_container.gd
+++ scripts/interface/player/left_container.gd
@@ -41,10 +41,35 @@ func mouse_interaction(state: String, container: HBoxContainer) -> void:
 			pass
 		"exited":
 			container.modulate.a = 1.0
-			#stats_info.play_animation("hide_container")
+			stats_info.play_animation("hide_container")
 			
 			
 			
 func update_stats_info_container(stats: String) -> void:
-	#stats_info.update_container(stats)
-	pass
+	stats_info.update_container(stats)
+
+
+func update_stats(stats_list: Array, bonus_stats_list: Array) -> void:
+	for index in grid_container.get_child_count():
+		var target_stat_text: Label = grid_container.get_child(index).get_node("Text")
+		var target_bonus_stat_text: Label = grid_container.get_child(index).get_node("Bonus")
+		
+		if bonus_stats_list[index] != 0:
+			target_stat_text.text = str(stats_list[index]) + " + "
+			target_bonus_stat_text.text = str(bonus_stats_list[index])
+		else:
+			target_stat_text.text = str(stats_list[index])
+			target_bonus_stat_text.text = ""
+
+
+func update_bonus_stats(bonus_dict: Dictionary, state: bool) -> void:
+	for key in bonus_dict.keys():
+		get_tree().call_group("player_stats", "update_bonus_stats", key, bonus_dict[key], state)
+		
+		
+func reset() -> void:
+	for container in grid_container.get_children():
+		if container.modulate.a != 1.0:
+			container.modulate.a = 1.0
+			stats_info.play_animation("hide_container")
+		
