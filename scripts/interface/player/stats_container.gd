e3faa18513e86910cb27c0ce0ffc966120099d48
FastShowDev <zlFastShow@hotmail.com>
2023-01-06 02:18:57 -0300

    Create initial script for stats_container

---
+++ scripts/interface/player/stats_container.gd
@@ -0,0 +1,29 @@
+extends Control
+class_name StatsContainer
+
+onready var animation: AnimationPlayer =  get_node("Animation")
+onready var left_container: TextureRect = get_node("LeftContainer")
+onready var right_container: TextureRect = get_node("RightContainer")
+
+var is_visible: bool = false
+
+
+func update_stats(stats_list: Array, bonus_stats_list: Array) -> void:
+	left_container.update_stats(stats_list, bonus_stats_list)
+
+
+func update_bonus_stats(bonus_dict: Dictionary, state: bool) -> void:
+	left_container.update_bonus_stats(bonus_dict, state)
+	
+	
+#Esconde a interface
+func reset() -> void:
+	left_container.reset()
+	right_container.reset()
+	
+
+func update_avaliable_points() -> void:
+	right_container.update_avaliable_points(5) #Valor fixo
+
+func _ready():
+	pass
